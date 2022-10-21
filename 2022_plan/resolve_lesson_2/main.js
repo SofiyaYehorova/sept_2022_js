d420553144c185185640fb8f2d4bd16cb3794323
SofiyaYehorova <yehorova66@gmail.com>
2022-10-21 11:08:36 +0300

    hw_2

---
+++ 2022_plan/resolve_lesson_2/main.js
@@ -0,0 +1,136 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+// let arr=['laptop', true, 15, 'table', 'chair', false, 10, 'inet', 'food', 'fire'];
+// console.log(arr);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+// let book1 = {
+//     title: 'велика книга почуттів',
+//     pageCount: 250,
+//     genre: 'оповідання для дітей'
+// }
+//
+// let book2 = {
+//     title: 'Динозаври',
+//     pageCount: 18,
+//     genre: 'для дітей'
+// }
+//
+// let book3 = {
+//     title: 'Морской ястреб',
+//     pageCount: 310,
+//     genre: 'пригоди'
+// }
+//
+// console.log(book1, book2, book3);
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив.
+// Кожен автор має поля name,age
+
+// let book1 = {
+//     title: 'велика книга почуттів',
+//     pageCount: 250,
+//     genre: 'оповідання для дітей',
+//     authors: [
+//         {
+//             name: 'Ґжеґож Касдепке',
+//             age: 50
+//         }
+//     ]
+// }
+//
+// let book2 = {
+//     title: 'Динозаври',
+//     pageCount: 18,
+//     genre: 'для дітей',
+//     authors: [
+//         {
+//             name: 'Тетяна Кузменко',
+//             age: 40
+//         }
+//     ]
+// }
+//
+// let book3 = {
+//     title: 'Морской ястреб',
+//     pageCount: 310,
+//     genre: 'пригоди',
+//     authors: [
+//         {
+//             name: 'Рафаель Сабатини',
+//             age: 60
+//         }
+//     ]
+// }
+//
+// console.log(book1, book2, book3);
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+// let arr_users = [
+//     user1 =
+//         {
+//             name: 'vasya',
+//             username: 'dfgs',
+//             password: 'fgdjdk'
+//         },
+//     user2 = {
+//         name: 'oleg',
+//         username: 'fgdjdk',
+//         password: 'fgdjdl'
+//     },
+//     user3 = {
+//         name: 'sfdgdks',
+//         username: 'djdadjal',
+//         password: 'akcahal'
+//     },
+//     user4 = {
+//         name: 'adalfla',
+//         username: 'djaflfl',
+//         password: 'akhflajlf'
+//     },
+//     user5 = {
+//         name: 'ad;ak;fa',
+//         username: 'akfjalfj',
+//         password: 'aljfla'
+//     },
+//     user6 = {
+//         name: 'aalfjaol',
+//         username: 'klajflaj',
+//         password: 'aldkakd'
+//     },
+//     user7 = {
+//         name: 'akjalfj',
+//         username: 'akdjkajfl',
+//         password: 'dkiafic'
+//     },
+//     user8 = {
+//         name: 'kcaklfjla',
+//         username: 'xkajldm',
+//         password: 'ladmlxjd'
+//     },
+//     user9 = {
+//         name: 'zalkd',
+//         username: 'dmajd',
+//         password: 'hxikand'
+//     },
+//     user10 = {
+//         name: 'ajdhwd',
+//         username: 'khandh',
+//         password: 'ixmwxd'
+//     }
+// ]
+//
+// console.log(user1['password']);
+// console.log(user2['password']);
+// console.log(user3['password']);
+// console.log(user4['password']);
+// console.log(user5['password']);
+// console.log(user6['password']);
+// console.log(user7['password']);
+// console.log(user8['password']);
+// console.log(user9['password']);
+// console.log(user10['password']);
+
