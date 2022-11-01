a87b2feea3798cb0270b0e38dcf8a0203e3b8ae8
SofiyaYehorova <yehorova66@gmail.com>
2022-11-01 10:29:06 +0200

    hw-1

--- 2022_plan/resolve_lesson_1/main.js
+++ 2022_plan/resolve_lesson_1/main.js
@@ -1,71 +1,47 @@
 // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
-//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+//     Вивести кожну змінну за допомогою: console.log
 
 // let a = 'hello';
 // console.log(a);
-// alert(a);
-// document.write(a);
 
 // let b = 'owu';
 // console.log(b);
-// alert(b);
-// document.write(b);
 
 // let c = 'com';
 // console.log(c);
-// alert(c);
-// document.write(c);
 
 // let d = 'ua';
 // console.log(d);
-// alert(d);
-// document.write(d);
 
 // let num1 = 1;
 // console.log(num1);
-// alert(num1);
-// document.write(num1);
 
 // let num2 = 10;
 // console.log(num2);
-// alert(num2);
-// document.write(num2);
+
 
 // let num3 = -999;
 // console.log(num3);
-// alert(num3);
-// document.write(num3);
 
 // let num4 = 123;
 // console.log(num4);
-// alert(num4);
-// document.write(num4);
+
 
 // let num5 = 3.14;
 // console.log(num5);
-// alert(num5);
-// document.write(num5);
 
 // let num6 = 2.7;
 // console.log(num6);
-// alert(num6);
-// document.write(num6);
 
 // let num7 = 16;
 // console.log(num7);
-// alert(num7);
-// document.write(num7);
 
 // let bool1 = true;
 // console.log(bool1);
-// alert(bool1);
-// document.write(bool1);
 
 // let bool2 = false;
 // console.log(bool2);
-// alert(bool2);
-// document.write(bool2);
-
+//
 
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person
 // (Не об'єкт, просто за допомоги конкатенації)
@@ -76,11 +52,9 @@
 
 // let person1 = lastName + ' ' + firstName + ' ' + middleName;
 // console.log(person1);
-// document.write(person1);
 
 // let person2=`${lastName} ${firstName} ${middleName}`;
 // console.log(person2);
-// document.write(person2);
 
 
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
