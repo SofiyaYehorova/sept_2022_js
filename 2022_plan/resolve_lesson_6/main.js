ffebbccc7719cd3ce07da5ef6903093553ea634a
SofiyaYehorova <yehorova66@gmail.com>
2022-11-03 11:57:05 +0200

    hw-6

--- 2022_plan/resolve_lesson_6/main.js
+++ 2022_plan/resolve_lesson_6/main.js
@@ -121,25 +121,107 @@
 // console.log(filter);
 
 // описати колоду карт
+
+let deckOfCards = [
+    {cardSuit: 'spade', value: '6', color: 'black'},
+    {cardSuit: 'spade', value: '7', color: 'black'},
+    {cardSuit: 'spade', value: '8', color: 'black'},
+    {cardSuit: 'spade', value: '9', color: 'black'},
+    {cardSuit: 'spade', value: '10', color: 'black'},
+    {cardSuit: 'spade', value: 'jack', color: 'black'},
+    {cardSuit: 'spade', value: 'queen', color: 'black'},
+    {cardSuit: 'spade', value: 'king', color: 'black'},
+    {cardSuit: 'spade', value: 'ace', color: 'black'},
+    {cardSuit: 'spade', value: 'joker', color: 'black'},
+
+    {cardSuit: 'diamond', value: '6', color: 'red'},
+    {cardSuit: 'diamond', value: '7', color: 'red'},
+    {cardSuit: 'diamond', value: '8', color: 'red'},
+    {cardSuit: 'diamond', value: '9', color: 'red'},
+    {cardSuit: 'diamond', value: '10', color: 'red'},
+    {cardSuit: 'diamond', value: 'jack', color: 'red'},
+    {cardSuit: 'diamond', value: 'queen', color: 'red'},
+    {cardSuit: 'diamond', value: 'king', color: 'red'},
+    {cardSuit: 'diamond', value: 'ace', color: 'red'},
+    {cardSuit: 'diamond', value: 'joker', color: 'red'},
+
+    {cardSuit: 'heard', value: '6', color: 'red'},
+    {cardSuit: 'heard', value: '7', color: 'red'},
+    {cardSuit: 'heard', value: '8', color: 'red'},
+    {cardSuit: 'heard', value: '9', color: 'red'},
+    {cardSuit: 'heard', value: '10', color: 'red'},
+    {cardSuit: 'heard', value: 'jack', color: 'red'},
+    {cardSuit: 'heard', value: 'queen', color: 'red'},
+    {cardSuit: 'heard', value: 'king', color: 'red'},
+    {cardSuit: 'heard', value: 'ace', color: 'red'},
+    {cardSuit: 'heard', value: 'joker', color: 'red'},
+
+    {cardSuit: 'clubs', value: '6', color: 'black'},
+    {cardSuit: 'clubs', value: '7', color: 'black'},
+    {cardSuit: 'clubs', value: '8', color: 'black'},
+    {cardSuit: 'clubs', value: '9', color: 'black'},
+    {cardSuit: 'clubs', value: '10', color: 'black'},
+    {cardSuit: 'clubs', value: 'jack', color: 'black'},
+    {cardSuit: 'clubs', value: 'queen', color: 'black'},
+    {cardSuit: 'clubs', value: 'king', color: 'black'},
+    {cardSuit: 'clubs', value: 'ace', color: 'black'},
+    {cardSuit: 'clubs', value: 'joker', color: 'black'}
+];
+
 // - знайти піковий туз
+// const findAce = deckOfCards.find(card => card.cardSuit === 'spade' && card.value === 'ace');
+// console.log(findAce);
+
 // - всі шістки
+
+// const filterOfSix = deckOfCards.filter(card => card.value === '6');
+// console.log(filterOfSix);
+
 // - всі червоні карти
+
+// const filterOfRed = deckOfCards.filter(card => card.color === 'red');
+// console.log(filterOfRed);
+
 // - всі буби
+
+// const filterOfDiamond = deckOfCards.filter(card => card.cardSuit === 'diamond');
+// console.log(filterOfDiamond);
+
 // - всі трефи від 9 та більше
-//
-//
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const filterOfClubs = deckOfCards.filter(card => card.cardSuit === 'clubs' && card.value >= 9 ||
+//     typeof card.value === 'string' && card.cardSuit === 'clubs');
+// console.log(filterOfClubs);
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 // {
 //     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
 //         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
 //     color:'', // 'red','black'
 // }
-//
+
+
 //
 // Додатково по reduce
 // Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
 // {
 //     spades:[],
-//         diamonds:[],
+//     diamonds:[],
 //     hearts:[],
 //     clubs:[]
 // }
+
+const reduce = deckOfCards.reduce(function (accumulator, card) {
+    if (card.cardSuit === 'spade') {
+        accumulator.spades.push(card);
+    }if (card.cardSuit === 'diamond') {
+        accumulator.diamonds.push(card);
+    }if (card.cardSuit === 'heard') {
+        accumulator.heards.push(card);
+    } else if (card.cardSuit === 'clubs'){
+        accumulator.clubs.push(card)
+    }
+    return accumulator;
+}, {spades: [], diamonds: [], heards: [], clubs: []});
+
+console.log(reduce);
