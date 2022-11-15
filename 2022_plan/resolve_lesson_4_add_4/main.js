4500e584943a21a0b49ada293b514903464d79ad
SofiyaYehorova <yehorova66@gmail.com>
2022-11-15 11:19:00 +0200

    hw-4_add_4

--- 2022_plan/resolve_lesson_4_add_4/main.js
+++ 2022_plan/resolve_lesson_4_add_4/main.js
@@ -56,15 +56,15 @@
 
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-let arr1=[]
-for (let i = 0; i < arr.length; i += 3) {
-    if (arr[i] % 2 === 0) {
-        console.log(arr[i]);
-        arr.push(arr1[i])
-    }
-}
-console.log(arr1);
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// let arr1=[]
+// for (let i = 0; i < arr.length; i += 3) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i]);
+//         arr.push(arr1[i])
+//     }
+// }
+// console.log(arr1);
 
 // for (let i = 0; i < arr1.length; i++) {
 //     console.log(arr1[i]);
